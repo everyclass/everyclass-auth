2f80a7ed2873c499149a505471d21077d814a490
EmanuelGi <919081500@qq.com>
2018-11-14 16:30:33 +0800

    redis配置更新

---
+++ everyclass/auth/db/redisdb.py
@@ -0,0 +1,6 @@
+import redis
+from everyclass.auth.config import get_config
+config = get_config()
+print(config.REDIS_CONFIG)
+redis_client = redis.Redis(host=config['REDIS_CONFIG']['host'], port=config['REDIS_CONFIG']['port'])
+
