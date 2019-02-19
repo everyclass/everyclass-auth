85a43a9345832bd6f50efba9c87ef0210b90417a
Frederic Chan <frederic.t.chan@gmail.com>
2019-02-19 10:02:50 +0800

    change local compose yml

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ gc.set_threshold(0)
 gc.freeze()
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=5000)
 
     # gevent_server = gevent.pywsgi.WSGIServer(('', 80), app)
     # gevent_server.serve_forever()
